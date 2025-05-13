4e8169ab34e0b6861db09636e0259d5a75f4eec0
MarlonVieira <marlongabriel_100@hotmail.com>
2025-05-12 21:06:21 -0300

    01. A new project using the Spring Framework. 05/12/2025

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,9 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.SeriesData;
+import br.com.alura.screenmatch.service.ConsumptionAPI;
+import br.com.alura.screenmatch.service.ConvertData;
+import br.com.alura.screenmatch.service.IConvertData;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +17,15 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("TEST!");
+		var consumptionAPI = new ConsumptionAPI();
+		var json = consumptionAPI.getData("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
+		System.out.println(json);
+
+		//json = consumptionAPI.getData("https://coffee.alexflipnote.dev/random.json");
+		//System.out.println(json);
+
+		ConvertData convertData = new ConvertData();
+		SeriesData data = convertData.getData(json, SeriesData.class);
+		System.out.println(data);
 	}
 }
---
+++ src/main/java/br/com/alura/screenmatch/model/SeriesData.java
@@ -0,0 +1,11 @@
+package br.com.alura.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record SeriesData(String title,
+                         Integer totalSeasons,
+                         @JsonAlias("imdbRating") String Rating,
+                         String year) {
+}
